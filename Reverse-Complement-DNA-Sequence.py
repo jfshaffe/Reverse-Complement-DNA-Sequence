0ecbb3be8e5f02bb15f60a30796a71c91403c788
Joshua Shaffer <81334916+jfshaffe@users.noreply.github.com>
2021-03-29 22:15:22 -0700

    Update Reverse-Complement-DNA-Sequence.py

--- Reverse-Complement-DNA-Sequence.py
+++ Reverse-Complement-DNA-Sequence.py
@@ -1,5 +1,5 @@
 def main():   
-    k = input("DNA sequence ")
+    k = input("DNA sequence = ")
     s = k[::-1]
     b = s.upper()
     l = b.replace('G', 'Z')
@@ -8,5 +8,5 @@ def main():
     e = r.replace('T', 'A')
     p = e.replace('Z', 'C')
     u = p.replace('Q', 'T')
-    print(u)
+    print('reverse complement = ', u)
 main()
